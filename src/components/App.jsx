b91aff2572b7d11f0e22b661e13773e208cb0eb7
Mahathir003 <mahathirmahi03@gmail.com>
2026-09-14 14:51:47 +0600

    final touchups

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,10 @@
 import { useState, useEffect } from "react";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import Navbar from "./Navbar";
+import Navbar from "../Navbar";
 import Hero from "./Hero";
-import TechCard from "./TechCard";
-import StackSidebar from "./StackSidebar";
+import TechCard from "../TechCard";
+import StackSidebar from "../StackSidebar";
 import Footer from "./Footer";
 
 export default function App() {
